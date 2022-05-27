fa5865e8b879f69016212dca89884a3300206de8
daniel.zl <daniel.zl@meetsocial.com>
2022-05-27 10:21:48 +0800

    删除dap所有依赖

--- lua/lsp/config/rust.lua
+++ lua/lsp/config/rust.lua
@@ -27,16 +27,16 @@ local lspconfig_opts = {
 
 return {
   on_setup = function(server)
-    local ok_rt, rust_tools = pcall(require, "rust-tools")
-    if not ok_rt then
-      print("Failed to load rust tools, will set up `rust_analyzer` without `rust-tools`.")
-      server.setup(lspconfig_opts)
-    else
-      -- We don't want to call lspconfig.rust_analyzer.setup() when using rust-tools
-      rust_tools.setup({
-        server = lspconfig_opts,
-        dap = require("dap.nvim-dap.rust"),
-      })
-    end
+    -- local ok_rt, rust_tools = pcall(require, "rust-tools")
+    -- if not ok_rt then
+    --   print("Failed to load rust tools, will set up `rust_analyzer` without `rust-tools`.")
+    --   server.setup(lspconfig_opts)
+    -- else
+    --   -- We don't want to call lspconfig.rust_analyzer.setup() when using rust-tools
+    --   rust_tools.setup({
+    --     server = lspconfig_opts,
+    --     dap = require("dap.nvim-dap.rust"),
+    --   })
+    -- end
   end,
 }
