60c2990b3f564c801704cbb3a90a9e3281eae9bc
daniel.zl <daniel.zl@meetsocial.com>
2022-06-09 01:11:29 +0800

    python 语法

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -7,7 +7,7 @@ end
 treesitter.setup({
   -- 安装 language parser
   -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "tsx", "go" , "bash", "yaml"},
+  ensure_installed = { "json", "html", "css", "vim", "lua","python", "javascript", "typescript", "tsx", "go" , "bash", "yaml"},
   -- ensure_installed = "maintained",
 
   -- 启用代码高亮模块
